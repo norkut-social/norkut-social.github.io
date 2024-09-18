9f2c29ab98f589ccc80451803c23c70b96cac457
bitcao21 <bitcao21@proton.me>
2024-09-18 20:54:26 -0300

    update router

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,13 +4,13 @@ import HomeView from '../views/HomeView.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    //{
+    //  path: '/',
+    //  name: 'home',
+    //  component: HomeView
+    //},
     {
       path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/login',
       name: 'login',
       component: () => import('../views/LoginView.vue')
     },
